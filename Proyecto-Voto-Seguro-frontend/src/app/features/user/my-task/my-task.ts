cf34fe232659d0a6333cacd3a83e88082db93c6b
RuthRivera2004 <riveraruth861@gmail.com>
2025-12-10 23:55:27 -0600

    Angular setup

---
+++ Proyecto-Voto-Seguro-frontend/src/app/features/user/my-task/my-task.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-task',
+  imports: [],
+  templateUrl: './my-task.html',
+  styleUrl: './my-task.css',
+})
+export class MyTask {
+
+}
